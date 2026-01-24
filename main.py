6644dee08596156c4929f57c017f73c6e4255265
Yarbick <yarbilovilyas@gmail.com>
2026-01-24 17:13:03 +0300

    Создание main_menu

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import arcade
 
+from main_menu import MainMenu
+
 
 def main():
     window = arcade.Window(1000, 900, "Tower Defence")
+    main_menu_view = MainMenu()
+    main_menu_view.setup()
+    window.show_view(main_menu_view)
     arcade.run()
 
 
