fb85f2e88e49aab8aa98285a0fa12051f454a665
Yarbick <yarbilovilyas@gmail.com>
2026-01-24 22:30:05 +0300

    Создание и настройка кнопок, установка минимальных размеров

--- main_menu.py
+++ main_menu.py
@@ -1,25 +1,55 @@
 import arcade
-
+import arcade.gui
+from arcade.gui import UIOnClickEvent, UIFlatButton
+from arcade.gui.widgets.buttons import UIFlatButtonStyle
 
 TILE_SIZE = 64
 TILEMAP_SCALING = 2.0
 BACKGROUND_SPEED = 100
 
 
+class PlayButton(arcade.gui.UIFlatButton):
+    pass
+
+
+class ExitButton(arcade.gui.UIFlatButton):
+    def on_click(self, event: UIOnClickEvent) -> None:
+        self.parent.window.close()
+
+
 class MainMenu(arcade.View):
     def __init__(self):
         super().__init__()
 
+        arcade.load_font("resources/assets/fonts/CGXYZLCD/CGXYZLCD-Regular.otf")
+        self.logo_texture = arcade.load_texture("resources/assets/images/logo/logo_text.png")
+
+        self.world_camera: arcade.Camera2D = arcade.camera.Camera2D()
         self.background_list: arcade.SpriteList | None = None
-        self.screen_width: int = self.window.screen.width
-        self.screen_height: int = self.window.screen.height
         self.world_width: int | None = None
         self.world_height: int | None = None
 
-        self.world_camera: arcade.Camera2D = arcade.camera.Camera2D()
         self.gui_camera: arcade.Camera2D = arcade.camera.Camera2D()
-
-        self.logo_texture: arcade.Texture = arcade.load_texture("resources/assets/logo/logo_text.png")
+        self.screen_width: int = self.window.screen.width
+        self.screen_height: int = self.window.screen.height
+        self.ui_manager = arcade.gui.UIManager()
+        self.ui_manager.enable()
+        button_style = {
+            "normal": UIFlatButtonStyle(font_name="CGXYZ LCD", font_size=16, bg=(60, 60, 60)),
+            "hover": UIFlatButtonStyle(font_name="CGXYZ LCD", font_size=16, bg=(80, 80, 80)),
+            "press": UIFlatButtonStyle(font_name="CGXYZ LCD", font_size=16, bg=(100, 100, 100)),
+            "disabled": UIFlatButtonStyle(font_name="CGXYZ LCD", font_size=16, bg=(160, 160, 160))
+        }
+        self.play_button = PlayButton(
+            x=self.screen_width * 0.5, y=self.screen_height * 0.7 - 125,
+            width=256, height=64, text="PLAY", style=button_style
+        )
+        self.ui_manager.add(self.play_button)
+        self.exit_button = ExitButton(
+            x=self.screen_width * 0.5, y=self.screen_height * 0.7 - 200,
+            width=256, height=64, text="EXIT", style=button_style
+        )
+        self.ui_manager.add(self.exit_button)
 
     def setup(self) -> None:
         tilemap: arcade.TileMap = arcade.load_tilemap("resources/levels/menu.tmx", scaling=TILEMAP_SCALING)
@@ -38,8 +68,9 @@ class MainMenu(arcade.View):
         self.gui_camera.use()
         arcade.draw_texture_rect(
             self.logo_texture,
-            arcade.Rect(0, 0, 0, 0, 620, 64, self.screen_width * 0.5, self.world_height * 0.5)
+            arcade.Rect(0, 0, 0, 0, 620, 64, self.screen_width * 0.5, self.screen_height * 0.7)
         )
+        self.ui_manager.draw()
 
     def on_update(self, delta_time: float) -> None:
         self.world_camera.position = (
@@ -55,3 +86,6 @@ class MainMenu(arcade.View):
         self.world_camera.match_window()
         self.gui_camera.match_window()
         self.gui_camera.position = self.screen_width * 0.5, self.screen_height * 0.5
+
+        self.play_button.center_x, self.play_button.center_y = self.screen_width * 0.5, self.screen_height * 0.7 - 125
+        self.exit_button.center_x, self.exit_button.center_y = self.screen_width * 0.5, self.screen_height * 0.7 - 200
