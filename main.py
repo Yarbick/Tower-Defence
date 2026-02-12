1bc4641dcb1f372806e127802b7370d216cb8a89
Yarbick <yarbilovilyas@gmail.com>
2026-02-12 20:19:30 +0300

    Респрайт башен, добавление иконки приложения
    main.py:
    1. Создание и отображение иконки приложения

--- main.py
+++ main.py
@@ -2,18 +2,22 @@
 
 # Графика
 import arcade
+from pyglet.image import load as load_image
 # Главное меню
 from scripts.scenes.main_menu import MainMenu
 
 
 def main() -> None:
     # Окно
-    window = arcade.Window(1000, 800, "Tower Defence", resizable=True)
+    window: arcade.Window = arcade.Window(1000, 800, "Tower Defence", resizable=True)
     window.set_minimum_size(800, 600)
     window.set_maximum_size(1920, 1080)
+    # Иконка
+    icon_image = load_image("resources/assets/images/logo/logo_icon.png")
+    window.set_icon(icon_image)
 
     # Главное меню
-    main_menu_view = MainMenu()
+    main_menu_view: MainMenu = MainMenu()
     main_menu_view.setup()
     window.show_view(main_menu_view)
 
