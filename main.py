206e762c9c008a812190173b48e75d198f2efc90
Yarbick <yarbilovilyas@gmail.com>
2026-02-14 16:05:20 +0300

    Изменение минимальных размеров окна, добавление README.txt

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from content.scripts.scenes.main_menu import MainMenu
 def main() -> None:
     # Окно
     window: arcade.Window = arcade.Window(1000, 800, "Tower Defence", resizable=True)
-    window.set_minimum_size(800, 600)
+    window.set_minimum_size(900, 600)
     window.set_maximum_size(1920, 1080)
     # Иконка
     icon_image = load_image("content/resources/assets/images/logo/logo_icon.png")
